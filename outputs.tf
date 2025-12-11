408a0fa9339f9c37a3e7120273a86200e7338285
Ashwa <ashwajce@gmail.com>
2025-12-11 09:34:33 +0100

    CloudForgeX: Update outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "storage_account_id" {
+  description = "The ID of the created storage account."
+  value       = azurerm_storage_account.main.id
+}
+
+output "storage_account_name" {
+  description = "The name of the created storage account."
+  value       = azurerm_storage_account.main.name
+}
+
+output "resource_group_name" {
+  description = "The name of the resource group."
+  value       = azurerm_resource_group.main.name
+}
+
+output "primary_blob_endpoint" {
+  description = "The primary blob endpoint of the storage account."
+  value       = azurerm_storage_account.main.primary_blob_endpoint
+}
